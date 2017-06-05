5d988a43e4b55f98473dd7f39ba9eb9a0f79d0ec
Manasi Chaudhari <chaudhari.m@husky.neu.edu>
2017-06-04 21:16:52 -0700

    Assignment 2

--- CloudNodeJs.js
+++ CloudNodeJs.js
@@ -6,10 +6,8 @@ var express = require('express');
 //initialize app as an express application
 var app = express();
 
-var port = process.env.PORT || 3000;
-
+app.set('port', (process.env.PORT || 5000));
 app.use(express.static(__dirname+'/public'));
-app.listen(port);
-
-console.log("hello world!Port is" + port);
-
+app.listen(app.get('port'), function() {
+    console.log('Node app is running on port', app.get('port'));
+});
