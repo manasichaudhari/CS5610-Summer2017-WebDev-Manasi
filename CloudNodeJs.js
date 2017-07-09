1a0d5ae85c414c30dd731ee28f3c196b884a24e0
Manasi Chaudhari <chaudhari.m@husky.neu.edu>
2017-07-08 21:19:52 -0700

    Test font awesome

--- CloudNodeJs.js
+++ CloudNodeJs.js
@@ -10,7 +10,7 @@ app.use(app.express.static(__dirname+'/public'));
 
 require("./assignment/app");
 
-app.set('port', (process.env.PORT || 7000));
+app.set('port', (process.env.PORT || 3000));
 app.listen(app.get('port'), function() {
     console.log('Node app is running on port', app.get('port'));
 });
